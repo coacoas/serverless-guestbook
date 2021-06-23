5dfd993c0403d22d699b2f4850dfa9801d0a8f17
Bill Carlson <bill@coacoas.net>
2021-06-23 10:05:31 -0400

    Set base URI

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://bbf92b8f.us-east.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
